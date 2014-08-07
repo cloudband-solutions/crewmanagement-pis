f9f1d558ccb9f1ca43a4b948347feaf073e991da
Raphael Alampay <raphael.alampay@gmail.com>
2014-08-07 17:28:56 +0800

    fix on crew show

--- app/models/educational_attainment.rb
+++ app/models/educational_attainment.rb
@@ -1,2 +1,9 @@
 class EducationalAttainment < ActiveRecord::Base
+  validates :year_graduated, presence: true
+  validates :school, presence: true
+  validates :course_finished, presence: true
+
+  def to_s
+    "#{year_graduated} - #{school} - #{course_finished}"
+  end
 end
