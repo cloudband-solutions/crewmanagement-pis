32298af5a59fb36ade5fc4aecb67c1883d63c04f
Raphael Alampay <raphael.alampay@gmail.com>
2014-11-02 17:32:59 +0800

    use wkhtmltopdf.sh in server

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,7 @@
 WickedPdf.config = {
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   #:layout => "pdf.html",
-  :exe_path => '/usr/bin/wkhtmltopdf'
+  # use script in deployment server
+  exe_path: '/usr/local/bin/wkhtmltopdf.sh'
+  #:exe_path => '/usr/bin/wkhtmltopdf'
 }
