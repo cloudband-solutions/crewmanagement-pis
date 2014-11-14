3510478441cbbca6d5a354dabbda876e21e2383c
kathleenlogmao <kathleenlogmao@gmail.com>
2014-11-14 20:06:00 +0800

    Trans

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,7 +1,9 @@
+# NOTE: Script for wkhtmltopdf.sh
+# xvfb-run -a -s "-screen 0 1366x786x16" wkhtmltopdf "$@"
 WickedPdf.config = {
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   #:layout => "pdf.html",
   # use script in deployment server
-  #exe_path: '/usr/local/bin/wkhtmltopdf.sh'
-  :exe_path => '/usr/bin/wkhtmltopdf'
+  exe_path: '/usr/local/bin/wkhtmltopdf.sh'
+  # :exe_path => '/usr/bin/wkhtmltopdf'
 }
