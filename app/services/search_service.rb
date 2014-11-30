9cb040454312d53b781391f2e0490623fd04d742
Raphael Alampay <raphael.alampay@gmail.com>
2014-11-30 23:10:47 +0800

    fixed bug in advanced search for joins

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -2,30 +2,30 @@ class SearchService
   def self.advanced_search(query)
     crews = Crew.active
 
-    if !query[:name].blank?
+    if !query[:name].blank? && !crews.blank?
       query[:name].split(" ").each do |n|
         crews = crews.where("crews.firstname LIKE :q OR crews.middlename LIKE :q OR crews.lastname LIKE :q OR crews.code_number LIKE :q", q: "%#{n}%")
       end
     end
 
-    if !query[:vessel].blank?
-      crews = crews.where("id IN (?)", EmploymentRecord.where(vessel_id: query[:vessel]).pluck(:crew_id).uniq)
+    if !query[:school].blank? && !crews.blank?
+      crews = crews.joins(:educational_attainments).where("educational_attainments.school = ?", query[:school])
     end
 
-    if !query[:rank].blank?
-      crews = crews.where("id IN (?)", EmploymentRecord.where(rank_id: query[:rank]).pluck(:crew_id).uniq)
-    end
-
-    if !query[:principal].blank?
-      crews = crews.where("id IN (?)", EmploymentRecord.where(principal_id: query[:principal]).pluck(:crew_id).uniq)
-    end
-
-    if !query[:license_type].blank?
+    if !query[:license_type].blank? && !crews.blank?
       crews = crews.joins(:licenses).where("licenses.license_type_id = ?", query[:license_type])
     end
 
-    if !query[:school].blank?
-      crews = crews.joins(:educational_attainments).where("educational_attainments.school = ?", query[:school])
+    if !query[:vessel].blank? && !crews.blank?
+      crews = crews.where("crews.id IN (?)", EmploymentRecord.where(vessel_id: query[:vessel]).pluck(:crew_id).uniq)
+    end
+
+    if !query[:rank].blank? && !crews.blank?
+      crews = crews.where("crews.id IN (?)", EmploymentRecord.where(rank_id: query[:rank]).pluck(:crew_id).uniq)
+    end
+
+    if !query[:principal].blank? && !crews.blank?
+      crews = crews.where("crews.id IN (?)", EmploymentRecord.where(principal_id: query[:principal]).pluck(:crew_id).uniq)
     end
 
     crews
