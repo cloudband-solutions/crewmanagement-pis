63d3cdbc745b1fa15927972ff7b7d8d6d5934bfb
Raphael Alampay <raphael.alampay@gmail.com>
2015-03-20 08:41:09 +0800

    removed validation for crew code and relative info

--- app/models/crew.rb
+++ app/models/crew.rb
@@ -61,7 +61,7 @@ class Crew < ActiveRecord::Base
   accepts_nested_attributes_for :crew_vessel_evaluations, allow_destroy: true
 
   validates :date_employed, presence: true
-  validates :code_number, presence: true, uniqueness: true
+  #validates :code_number, presence: true, uniqueness: true
   validates :firstname, presence: true
   validates :lastname, presence: true
   validates :middlename, presence: true
@@ -73,9 +73,9 @@ class Crew < ActiveRecord::Base
   validates :eye_color, presence: true
   validates :shoe_size, presence: true
   validates :cloth_size, presence: true
-  validates :nearest_relative_name, presence: true
-  validates :nearest_relative_relationship, presence: true
-  validates :nearest_relative_address, presence: true
+  #validates :nearest_relative_name, presence: true
+  #validates :nearest_relative_relationship, presence: true
+  #validates :nearest_relative_address, presence: true
   validates :address, presence: true
   validates :civil_status, presence: true, inclusion: { in: Crew::CIVIL_STATUSES }
   validates :nationality, presence: true
